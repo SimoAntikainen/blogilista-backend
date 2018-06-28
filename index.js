59b49196c79b9a616f4b1ea0f2e7073ae454b291
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-29 00:37:14 +0300

    part 4.2 final

--- index.js
+++ index.js
@@ -25,26 +25,7 @@ mongoose
 
 app.use(cors())
 app.use(bodyParser.json())  
-
 app.use(middleware.logger)
-
-/**app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})**/
 
 app.use('/api/blogs', blogsRouter)
 
